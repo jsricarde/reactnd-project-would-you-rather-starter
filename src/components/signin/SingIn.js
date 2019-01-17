f08f613a7e4959ee11ba3c501b66959a1314ab3c
jsricarde <jsricarde@gmail.com>
2019-01-16 22:54:06 -0500

    Fix code review comments

--- src/components/signin/SingIn.js
+++ src/components/signin/SingIn.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {handleSetAuthedUser} from '../../actions/authedUser';
+import { connect } from 'react-redux';
+import { handleSetAuthedUser } from '../../actions/authedUser';
 import {
   Form, Button, Select
 } from 'antd';
@@ -14,14 +14,14 @@ const SingIn = props => {
     e.preventDefault();
     props.form.validateFields((err, values) => {
       if (!err) {
-        const {dispatch} = props
+        const { dispatch } = props
         dispatch(handleSetAuthedUser(values.userId))
       }
     });
   }
-  const {getFieldDecorator} = props.form;
+  const { getFieldDecorator } = props.form;
 
-  const {usersArray} = props
+  const { usersArray } = props
   return (
     <Form onSubmit={handleSubmit} className="signin-form">
       <h1>Welcome to the Would you rather app!</h1>
@@ -30,10 +30,10 @@ const SingIn = props => {
       <h1>Sign In</h1>
       <Form.Item>
         {getFieldDecorator('userId', {
-          rules: [{required: true, message: 'Please choose an user'}],
+          rules: [ { required: true, message: 'Please choose an user' } ],
         })(
-          <Select className="signin-form-select" placeholder="Select an user" style={{width: 200}}>
-            {usersArray.map((user) => (<Option value={user.id}>{user.name}</Option>))}
+          <Select className="signin-form-select" placeholder="Select an user" style={{ width: 200 }}>
+            {usersArray.map((user) => (<Option key={user.id} value={user.id}>{user.name}</Option>))}
           </Select>
         )}
       </Form.Item>
@@ -46,12 +46,12 @@ const SingIn = props => {
   );
 }
 
-function mapStateToProps({users}) {
+function mapStateToProps({ users }) {
   const usersArray = Object.values(users)
   return {
     usersArray
   }
 }
-const WrappedSingIn = Form.create({name: 'signin'})(SingIn);
+const WrappedSingIn = Form.create({ name: 'signin' })(SingIn);
 
 export default connect(mapStateToProps)(WrappedSingIn);
